3f8b21bee25eafb2b5ddedfdaa163e73e5f1034d
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-07-21 16:51:14 +0300

    Don't install slack-notifier if already installed

--- run.sh
+++ run.sh
@@ -14,13 +14,16 @@ if which ruby > /dev/null 2>&1 ; then
   echo "Install User: ${CURRENT_USER}"
   echo ""
 
-
-  if [ "${CURRENT_USER}" = "${RUBY_OWNER}" ]; then
-    echo "Installing slack-notifier..."
-         gem install slack-notifier -v 1.2.1 --no-ri --no-rdoc
+  if ! gem list | grep -q slack-notifier; then
+    if [ "${CURRENT_USER}" = "${RUBY_OWNER}" ]; then
+      echo "Installing slack-notifier..."
+      gem install slack-notifier --no-ri --no-rdoc
+    else
+      echo "Installing slack-notifier as root..."
+      sudo gem install slack-notifier --no-ri --no-rdoc
+    fi
   else
-    echo "Installing slack-notifier as root..."
-    sudo gem install slack-notifier -v 1.2.1 --no-ri --no-rdoc
+      echo "Skipping installing slack-notifier"
   fi
 
   $WERCKER_STEP_ROOT/run.rb
